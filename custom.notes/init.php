b35ece1ffb2290ec01cef2756ddf4238e1f81bbc
maliev30 <murad_2008.88@mail.ru>
2026-02-05 08:48:37 +0300

    www

--- custom.notes/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Custom\Notes\NotesApi;
-\Bitrix\Main\Loader::includeModule('custom.notes');
-\Bitrix\Main\Rest\RestServer::onBuildDescriptionRegister(NotesApi::OnRestServiceBuildDescription());
