7d184ffb464cb0a47233431c4382c5d14c2b876f
maliev30 <murad_2008.88@mail.ru>
2026-02-05 08:59:22 +0300

    www

--- custom.notes/install/components/custom-notes/notes/templates/.default/template.php
+++ custom.notes/install/components/custom-notes/notes/templates/.default/template.php
@@ -2,67 +2,6 @@
 
 <div id="notes-app"></div>
 <script type="module">
-    import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js';
-    import axios from 'https://cdn.jsdelivr.net/npm/axios@1.5.0/dist/esm/axios.min.js';
-    const App = {
-        setup() {
-            const notes = ref([]);
-            const title = ref('');
-            const content = ref('');
-
-            const loadNotes = async () => {
-                const res = await axios.get('/api/notes_list.php');
-                notes.value = res.data;
-            };
-
-            const addNote = async () => {
-                await axios.post('/api/notes_create.php', new URLSearchParams({
-                    title: title.value,
-                    content: content.value
-                }));
-                title.value = content.value = '';
-                await loadNotes();
-            };
-
-            const updateNote = async (note) => {
-                await axios.post('/api/notes_update.php', new URLSearchParams({
-                    ID: note.ID,
-                    title: note.TITLE,
-                    content: note.CONTENT
-                }));
-                await loadNotes();
-            };
-
-            const deleteNote = async (id) => {
-                await axios.post('/api/notes_delete.php', new URLSearchParams({ ID: id }));
-                await loadNotes();
-            };
-
-            onMounted(loadNotes);
-
-            return { notes, title, content, addNote, updateNote, deleteNote };
-        },
-        template: `
-        <div class="notes_form_container">
-            <h2>Notes</h2>
-            <div class="note-form">
-                <input v-model="title" type="text" name="notes_title" placeholder="Title" />
-                <textarea v-model="content" name="notes_content" placeholder="Content"></textarea>
-                <button @click="addNote">Add Note</button>
-            </div>
-            <div class="form_result">
-                <ul class="notes-list">
-                    <li class="item_notes" v-for="note in notes" :key="note.ID">
-                        <b contenteditable @blur="updateNote({...note, TITLE: $event.target.innerText})">{{ note.TITLE }}</b>
-                        <span contenteditable @blur="updateNote({...note, CONTENT: $event.target.innerText})">{{ note.CONTENT }}</span>
-                        <button @click="deleteNote(note.ID)">Delete</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    `
-    };
-
-    createApp(App).mount('#notes-app');
-
+    import { initNotesApp } from '/local/modules/custom.notes/js/app.js';
+    initNotesApp('#notes-app');
 </script>
--- custom.notes/js/app.js
+++ custom.notes/js/app.js
@@ -1,63 +1,86 @@
-import { createApp, ref, onMounted } from 'vue';
-import axios from 'axios';
+import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js';
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.5.0/dist/esm/axios.min.js';
 
-const App = {
-    setup() {
-        const notes = ref([]);
-        const title = ref('');
-        const content = ref('');
+export function initNotesApp(selector = '#notes-app') {
+    const App = {
+        setup() {
+            const notes = ref([]);
+            const title = ref('');
+            const content = ref('');
 
-        const loadNotes = async () => {
-            const res = await axios.get('/api/notes_list.php');
-            notes.value = res.data;
-        };
+            const loadNotes = async () => {
+                const res = await axios.get('/api/notes_list.php');
+                notes.value = res.data;
+            };
 
-        const addNote = async () => {
-            await axios.post('/api/notes_create.php', new URLSearchParams({
-                title: title.value,
-                content: content.value
-            }));
-            title.value = content.value = '';
-            await loadNotes();
-        };
+            const addNote = async () => {
+                if (!title.value.trim()) return;
 
-        const updateNote = async (note) => {
-            await axios.post('/api/notes_update.php', new URLSearchParams({
-                ID: note.ID,
-                title: note.TITLE,
-                content: note.CONTENT
-            }));
-            await loadNotes();
-        };
+                await axios.post('/api/notes_create.php', new URLSearchParams({
+                    title: title.value,
+                    content: content.value
+                }));
 
-        const deleteNote = async (id) => {
-            await axios.post('/api/notes_delete.php', new URLSearchParams({ ID: id }));
-            await loadNotes();
-        };
+                title.value = '';
+                content.value = '';
+                await loadNotes();
+            };
 
-        onMounted(loadNotes);
+            const updateNote = async (note) => {
+                await axios.post('/api/notes_update.php', new URLSearchParams({
+                    ID: note.ID,
+                    title: note.TITLE,
+                    content: note.CONTENT
+                }));
+            };
 
-        return { notes, title, content, addNote, updateNote, deleteNote };
-    },
-    template: `
+            const deleteNote = async (id) => {
+                await axios.post('/api/notes_delete.php', new URLSearchParams({
+                    ID: id
+                }));
+                await loadNotes();
+            };
+
+            onMounted(loadNotes);
+
+            return {
+                notes,
+                title,
+                content,
+                addNote,
+                updateNote,
+                deleteNote
+            };
+        },
+
+        template: `
         <div class="notes_form_container">
             <h2>Notes</h2>
+
             <div class="note-form">
-                <input v-model="title" type="text" name="notes_title" placeholder="Title" />
-                <textarea v-model="content" name="notes_content" placeholder="Content"></textarea>
+                <input v-model="title" placeholder="Title" />
+                <textarea v-model="content" placeholder="Content"></textarea>
                 <button @click="addNote">Add Note</button>
             </div>
-            <div class="form_result">
-                <ul class="notes-list">
-                    <li class="item_notes" v-for="note in notes" :key="note.ID">
-                        <b contenteditable @blur="updateNote({...note, TITLE: $event.target.innerText})">{{ note.TITLE }}</b>
-                        <span contenteditable @blur="updateNote({...note, CONTENT: $event.target.innerText})">{{ note.CONTENT }}</span>
-                        <button @click="deleteNote(note.ID)">Delete</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    `
-};
 
-createApp(App).mount('#notes-app');
+            <ul class="notes-list">
+                <li v-for="note in notes" :key="note.ID" class="item_notes">
+                    <b contenteditable
+                       @blur="updateNote({ ...note, TITLE: $event.target.innerText })">
+                       {{ note.TITLE }}
+                    </b>
+
+                    <span contenteditable
+                          @blur="updateNote({ ...note, CONTENT: $event.target.innerText })">
+                          {{ note.CONTENT }}
+                    </span>
+
+                    <button @click="deleteNote(note.ID)">Delete</button>
+                </li>
+            </ul>
+        </div>
+        `
+    };
+
+    createApp(App).mount(selector);
+}
